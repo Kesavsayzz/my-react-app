deacf687da4bafd012f0f30780d2e0a75aa5be86
Sai Kesava Konaparthi <saikesava159@gmail.com>
2023-10-26 16:29:01 +0530

    this is my base commit

---
+++ src/comps/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello World..!</h1>
+            <p>Welcome to the most important topic for a Front-end or for a Full Stack Developer who want to build their career into heights..!</p>
+        </div>
+    )
+}
+
+export default HelloWorld;
