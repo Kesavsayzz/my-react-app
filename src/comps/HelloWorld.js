d80c893d7449d07b5df28c6094c574151d2db894
Sai Kesava Konaparthi <saikesava159@gmail.com>
2023-10-26 16:36:41 +0530

    this is my first real commit

--- src/comps/HelloWorld.js
+++ src/comps/HelloWorld.js
@@ -5,6 +5,11 @@ function HelloWorld() {
         <div>
             <h1>Hello World..!</h1>
             <p>Welcome to the most important topic for a Front-end or for a Full Stack Developer who want to build their career into heights..!</p>
+            <div>
+                <h3>What is React..!?</h3>
+                <p>React is an Open source fornt-end library which is developed to build UI designs.
+                    React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.</p>
+            </div>
         </div>
     )
 }
